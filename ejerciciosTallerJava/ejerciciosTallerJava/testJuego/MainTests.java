0f042e019c4e9a05d9a709d39a9f76c4181618ac
Diego <dieguito98ig@gmail.com>
2020-11-04 23:20:35 -0300

    hola jony

--- ejerciciosTallerJava/ejerciciosTallerJava/testJuego/MainTests.java
+++ ejerciciosTallerJava/ejerciciosTallerJava/testJuego/MainTests.java
@@ -1,25 +1,40 @@
 package testJuego;
 
+import java.awt.EventQueue;
 import java.util.ArrayList;
 
 import game.Jugador;
 import game.Partida;
+import game.Tablero;
 
 public class MainTests {
 
 	public static void main(String[] args) {
-
 		ArrayList<Jugador> jugadores;
 		jugadores = new ArrayList<>();
 		jugadores.add(new Jugador("Dieguen", 3));
 		jugadores.add(new Jugador("Lucardo", 2));
 		Partida partida = new Partida(3, 2, jugadores);
 		partida.iniciarPartida();
+		
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Tablero frame = new Tablero(jugadores);
+					frame.setVisible(true);
+					frame.init();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+		
 
-		jugadores.get(0).ganarRonda(partida.getAfecto());
-		jugadores.get(0).ganarRonda(partida.getAfecto());
-		jugadores.get(0).ganarRonda(partida.getAfecto());
-		jugadores.get(0).ganarRonda(partida.getAfecto());
+//
+//		jugadores.get(0).ganarRonda(partida.getAfecto());
+//		jugadores.get(0).ganarRonda(partida.getAfecto());
+//		jugadores.get(0).ganarRonda(partida.getAfecto());
+//		jugadores.get(0).ganarRonda(partida.getAfecto());
 
 //		System.out.println(jugadores.get(0));
 //		partida.getMazo().darCarta(jugadores.get(0));
