2399d3d616a3d5770bd521c92864a13ebfce726f
Lucas <lucasdperez.96@gmail.com>
2020-11-28 03:08:19 -0300

    Tablero con música, para tirar magia numas

--- ejerciciosTallerJava/ejerciciosTallerJava/testJuego/MainTests.java
+++ ejerciciosTallerJava/ejerciciosTallerJava/testJuego/MainTests.java
@@ -1,7 +1,16 @@
 package testJuego;
 
 import java.awt.EventQueue;
+import java.io.File;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+
+import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
+import javax.sound.sampled.LineUnavailableException;
+import javax.sound.sampled.UnsupportedAudioFileException;
 
 import InterfaceGrafica.ComenzarRonda;
 import InterfaceGrafica.Tablero;
@@ -10,7 +19,7 @@ import game.Partida;
 
 public class MainTests {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws MalformedURLException, LineUnavailableException, IOException, UnsupportedAudioFileException {
 	
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -22,8 +31,14 @@ public class MainTests {
 					e.printStackTrace();
 				}
 			}
+			
+			
 		});
-
+		Clip clip = AudioSystem.getClip();
+		clip.open(AudioSystem.getAudioInputStream(new File("sounds/music.wav")));
+		clip.start();
+		clip.loop(Clip.LOOP_CONTINUOUSLY);
+		
 	}
 
 }
